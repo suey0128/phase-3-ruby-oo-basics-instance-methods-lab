8e6157b9ecdcd7b6bcd122a894ce0ffee1c67111
suey0128 <syui0128@gmail.com>
2021-06-29 14:29:53 -0700

    commit tree ea88be128483a55befcda0ded1388427bc7adcc0

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+class Dog
+
+    def bark
+        puts "Woof!"
+    end
+
+    def sit 
+        puts "The Dog is sitting"
+    end
+end
+
+# fido = Dog.new
+# fido.bark #=> "Woof!" 
+
+# snoopy = Dog.new
+# snoopy.bark #=> "Woof!" 
+
+# binding.pry
